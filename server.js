8481f0aab8b85ca2abe8b1c74b99feac8d21d6a7
mcnicholasj <james.mcnicholas@cgi.com>
2020-09-18 11:22:18 +0100

    test API endpoints

---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create a database variable to reuse the connection pool
+var db;
+
+// Connect to DB
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function(err, client) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    
+    // Save database object
+    db = client.db();
+    console.log("DB connection established");
+    
+    // Initialize the app
+    var server = app.listen(process.env.PORT || 8080, function() {
+        var port = server.address.port;
+        console.log("App now running on port", port);
+    });
+});
+
+// API routes
+
+// Error handler function
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason)
+    res.status(code || 500).json({"error": message});
+}
+
+// Contacts
+app.get("/api/contacts", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.")
+        } else {
+            res.status(200).json(docs)
+        }
+    });
+});
+
+app.post("/api/contacts", function(req, res) {
+    var newContact = req.body;
+    newContact.createDate = new Date();
+    
+    if (!req.body.name) {
+        handleError(res, "Invalid user input", "Must provide name", 400);
+    } else {
+        db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc){
+            if (err) {
+                handleError(res, err.message, "Failed to create new contact");
+            }  else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
+});
+
+// Contacts by ID
+app.get("/api/contacts/:id", function(req, res) {
+    
+});
+
+app.put("/api/contacts/:id", function(req, res) {
+    
+});
+
+app.delete("/api/contacts/:id", function(req, res) {
+    
+});
